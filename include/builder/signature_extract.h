5a888062072138b16cc86240443c79ebd11761bd
Ajay Brahmakshatriya <ajaybr@mit.edu>
2020-04-02 00:12:50 -0400

    Changed signature extraction to use functors instead of std::function. This prevents extra copy of args

--- include/builder/signature_extract.h
+++ include/builder/signature_extract.h
@@ -9,24 +9,24 @@ template <typename...AllArgs>
 struct extract_signature;
 
 
-template <typename RetType, typename T, typename...FutureArgTypes>
-struct extract_signature<RetType, T, FutureArgTypes...> {
+template <typename ClassType, typename RetType, typename T, typename...FutureArgTypes>
+struct extract_signature<ClassType, RetType, T, FutureArgTypes...> {
 	template <typename...RestArgTypes>
 	struct from {
-		static std::function<void(void)> call(builder_context *context, int arg_count, std::function<RetType(RestArgTypes..., T, FutureArgTypes...)> func, RestArgTypes&...rest_args) {
+		static std::function<void(void)> call(builder_context *context, int arg_count, ClassType func, RestArgTypes&...rest_args) {
 			std::string arg_name = "arg" + std::to_string(arg_count);
 			T* new_arg = context->assume_variable<T>(arg_name);
-			return extract_signature<RetType, FutureArgTypes...>::template from<RestArgTypes..., T>::call(context, arg_count + 1, func, rest_args..., *new_arg);
+			return extract_signature<ClassType, RetType, FutureArgTypes...>::template from<RestArgTypes..., T>::call(context, arg_count + 1, func, rest_args..., *new_arg);
 		}
 	};
 };
 
 
-template <typename RetType>
-struct extract_signature<RetType> {
+template <typename ClassType, typename RetType>
+struct extract_signature<ClassType, RetType> {
 	template <typename...RestArgTypes>
 	struct from {
-		static std::function<void(void)> call(builder_context *context, int arg_count, std::function<RetType(RestArgTypes...)> func, RestArgTypes&...rest_args) {
+		static std::function<void(void)> call(builder_context *context, int arg_count, ClassType func, RestArgTypes&...rest_args) {
 			return [&, func] (void) {
 				func(rest_args...);
 			};
@@ -40,7 +40,7 @@ struct extract_signature_from_lambda: public extract_signature_from_lambda<declt
 template <typename ClassType, typename ReturnType, typename...Args>
 struct extract_signature_from_lambda<ReturnType(ClassType::*)(Args...) const> {	
 	static std::function<void(void)> from(builder_context* context, ClassType func) {
-		return extract_signature<ReturnType, Args...>::template from<>::call(context, 0, std::function<ReturnType(Args...)>(func));
+		return extract_signature<ClassType, ReturnType, Args...>::template from<>::call(context, 0, func);
 	}
 };
 }
