a645fd79825fe51f6fa450ce68adc4822c797daf
Ajay Brahmakshatriya <ajaybr@mit.edu>
2022-04-08 11:50:00 -0400

    Added support for dynamically loading simple C functions for execution

---
+++ include/builder/builder_dynamic.h
@@ -0,0 +1,65 @@
+#ifndef BUILDER_DYNAMIC_H
+#define BUILDER_DYNAMIC_H
+
+#include "builder/builder_context.h"
+#include "blocks/c_code_generator.h"
+#include "blocks/rce.h"
+#include <cstdio>
+#include <fstream>
+#include <unistd.h>
+#include <dlfcn.h>
+
+#include "gen/compiler_headers.h"
+
+namespace builder {
+
+template <typename FT, typename...ArgsT>
+auto compile_function(FT f, ArgsT...args) -> void* {
+	builder_context context;	
+	// Currently we will use an unconfigured context
+	// Can take in extra parameters or a context object
+	auto ast = context.extract_function_ast(f, "execute", args...);
+	// Proactively run RCE
+	block::eliminate_redundant_vars(ast);		
+
+	char base_name_c[] = GEN_TEMPLATE_NAME;
+	int fd = mkstemp(base_name_c);
+	if (fd < 0) {
+		assert(false && "Opening a temporary file failed\n");
+	}
+	
+	close(fd);
+
+	std::string base_name(base_name_c);	
+	std::string source_name = base_name + ".c";
+	std::string compiled_name = base_name + ".so";
+	
+	std::string compiler_name = COMPILER_PATH;
+	std::string compile_command = compiler_name + " -shared -O3 " + source_name + " -o " + compiled_name;
+	
+		
+	std::ofstream oss(source_name);	
+
+	block::c_code_generator::generate_code(ast, oss, 0);
+
+	oss.close();
+
+	int err = system(compile_command.c_str());
+	if (err != 0) {	
+		assert(false && "Compilation failed\n");
+	}
+	
+	void* handle = dlopen(compiled_name.c_str(), RTLD_NOW | RTLD_LOCAL);
+	if (!handle) {
+		assert(false && "Loading compiled module failed\n");
+	}
+	
+	void* function = dlsym(handle, "execute");
+	if (!function) {
+		assert(false && "Loading compiled module failed\n");
+	}
+	return function;
+}
+
+}
+#endif
