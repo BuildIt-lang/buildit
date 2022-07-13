eaca5743ba0cdc072bde5f1b5b4a74e478b60931
Ajay Brahmakshatriya <ajaybr@mit.edu>
2022-07-12 23:06:23 -0400

    Added TRACER_USE_LIBUNWIND and made TRACER_USE_FLIMITS the default behaviour

--- src/util/tracer.cpp
+++ src/util/tracer.cpp
@@ -1,75 +1,56 @@
 #include "util/tracer.h"
 #include "builder/builder_context.h"
 #include <string>
+
+#ifdef TRACER_USE_LIBUNWIND
+#define UNW_LOCAL_ONLY
+#include <libunwind.h>
+#endif
+
 namespace builder {
 extern void lambda_wrapper(void);
+extern void lambda_wrapper_close(void);
 }
 namespace tracer {
-static unsigned long long call_point = 0;
-
-static void set_call_point(void) {
+#ifdef TRACER_USE_LIBUNWIND
+tag get_offset_in_function_impl(builder::builder_context *current_builder_context) {
 	unsigned long long function = (unsigned long long)(void *)builder::lambda_wrapper;
-#ifdef TRACER_USE_FLIMITS
 	unsigned long long function_end = (unsigned long long)(void *)builder::lambda_wrapper_close;
-#endif
-	void *buffer[50];
-	int backtrace_size = backtrace(buffer, 50);
-	char **backtrace_functions = backtrace_symbols(buffer, backtrace_size);
-	int i;
-	bool found = false;
-	for (i = 0; i < backtrace_size; i++) {
-		unsigned long long address;
-#ifndef TRACER_USE_FLIMITS
-		unsigned int offset;
-#ifdef __linux
-		if (sscanf(backtrace_functions[i], "%*[^+]+%x) [%llx]", &offset,
-			   &address) != 2) {
-			// printf("Scanning of backtrace failed, result might be
-			// bad\n");
-			continue;
-		}
-#elif __APPLE__
-		if (sscanf(backtrace_functions[i], "%*[^+]+ %i", &offset) !=
-		    1) {
-			// printf("Scanning of backtrace failed, result might be
-			// bad\n");
-			continue;
-		}
-		address = (unsigned long long)buffer[i];
-#else
-#error Backtracer currently only supported for Linux and MacOS
-#endif
+	unw_context_t context;
+	unw_cursor_t cursor;
 
-		if (function == address - offset) {
-			found = true;
+	unw_getcontext(&context);
+	unw_init_local(&cursor, &context);
+
+	tag new_tag;
+	while (unw_step(&cursor)) {
+		unw_word_t ip;
+		unw_get_reg(&cursor, UNW_REG_IP, &ip);
+		if ((unsigned long long)ip >= function && (unsigned long long) ip < function_end)
 			break;
-		}
-#else
-		address = (unsigned long long)buffer[i];
-		
-		if (function <= address && address < function_end) {
-			found = true;
-			break;
-		}
-			
-#endif
+		new_tag.pointers.push_back((unsigned long long)ip);
 	}
-	if (!found)
-		assert(false && "Call point not found\n");
-	call_point = (unsigned long long)buffer[i + 1];
-	free(backtrace_functions);
+	// Now add snapshots of static vars
+	assert(current_builder_context != nullptr);
+	for (builder::tracking_tuple tuple :
+	     current_builder_context->static_var_tuples) {
+		new_tag.static_var_snapshots.push_back(tuple.snapshot());
+	}
+	return new_tag;
 }
+
+#else
 tag get_offset_in_function_impl(
     builder::builder_context *current_builder_context) {
-	if (call_point == 0) {
-		set_call_point();
-	}
+	unsigned long long function = (unsigned long long)(void *)builder::lambda_wrapper;
+	unsigned long long function_end = (unsigned long long)(void *)builder::lambda_wrapper_close;
+
 	void *buffer[50];
 	tag new_tag;
 	// First add the RIP pointers
 	int backtrace_size = backtrace(buffer, 50);
 	for (int i = 0; i < backtrace_size; i++) {
-		if ((unsigned long long)buffer[i] == call_point)
+		if ((unsigned long long)buffer[i] >= function && (unsigned long long) buffer[i] < function_end)
 			break;
 		new_tag.pointers.push_back((unsigned long long)buffer[i]);
 	}
@@ -82,7 +63,7 @@ tag get_offset_in_function_impl(
 	}
 	return new_tag;
 }
-
+#endif
 static unsigned long long unique_tag_counter = 0;
 tag get_unique_tag(void) {
 	tag new_tag;
