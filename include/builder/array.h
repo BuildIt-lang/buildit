aa1a8d94445dc410fd0664516d0ed40b32753d36
Ajay Brahmakshatriya <ajaybr@mit.edu>
2024-10-08 21:13:47 -0700

    Extended builder::arrays to support any types and not just dyn_vars

--- include/builder/array.h
+++ include/builder/array.h
@@ -5,103 +5,118 @@
 
 namespace builder {
 
+
+// If the array is of dyn_vars, we initialize with a 
+// builder, anything else and we directly initialize with T
+// This avoids unnecessary copies unless entirely necessary
+template <typename T>
+struct initializer_selector {
+	typedef const T type;
+};
+
+template <typename T>
+struct initializer_selector<dyn_var<T>> {
+	typedef builder type;
+};
+
+
+
 template <typename T, size_t size = 0>
-class dyn_arr {
+class array {
 private:
-	dyn_var<T> *m_arr = nullptr;
+	T *m_arr = nullptr;
 	size_t actual_size = 0;
 
 public:
-	dyn_arr() {
+	array() {
 		if (size) {
 			actual_size = size;
-			m_arr = (dyn_var<T> *)new char[sizeof(dyn_var<T>) * actual_size];
+			m_arr = (T *)new char[sizeof(T) * actual_size];
 			for (static_var<size_t> i = 0; i < actual_size; i++) {
-				new (m_arr + i) dyn_var<T>();
+				new (m_arr + i) T();
 			}
+
 			// static tags for array nodes need to be adjusted
 			// so they are treated different from each other despite
 			// being declared at the same location.
 			// dyn_arr are special case of vars that escape their static scope but still
 			// shouldn't be treated together
 			// We do this by adding additional metadata on all of them
-			for (static_var<size_t> i = 0; i < actual_size; i++) {
-				m_arr[i].block_var->template setMetadata<int>("allow_escape_scope", 1);
-			}
+			
+			// We are removing the metadata for allow_escape_scope because it is not being 
+			// used anyway right now. Enabling this would require us to set it as a variable
+			// inside the context so all the dynamic variables constructed in this block would have
+			// the metadata set
+
 		}
 	}
-	dyn_arr(const std::initializer_list<builder> &init) {
+	// We need a SFINAE constructor of anything that is convertible to T
+	// but let's stick with T for now
+	array(const std::initializer_list<typename initializer_selector<T>::type> &init) {
 		if (size) {
 			actual_size = size;
 		} else {
 			actual_size = init.size();
 		}
-		m_arr = (dyn_var<T> *)new char[sizeof(dyn_var<T>) * actual_size];
+		m_arr = (T *)new char[sizeof(T) * actual_size];
 		for (static_var<size_t> i = 0; i < actual_size; i++) {
 			if (i < init.size())
-				new (m_arr + i) dyn_var<T>(*(init.begin() + i));
+				new (m_arr + i) T(*(init.begin() + i));
 			else
-				new (m_arr + i) dyn_var<T>();
-		}
-		for (static_var<size_t> i = 0; i < actual_size; i++) {
-			m_arr[i].block_var->template setMetadata<int>("allow_escape_scope", 1);
+				new (m_arr + i) T();
 		}
 	}
+
+
 	void set_size(size_t new_size) {
 		assert(size == 0 && "set_size should be only called for dyn_arr without size");
 		assert(m_arr == nullptr && "set_size should be only called once");
 		actual_size = new_size;
-		m_arr = (dyn_var<T> *)new char[sizeof(dyn_var<T>) * actual_size];
+		m_arr = (T *)new char[sizeof(T) * actual_size];
 		for (static_var<size_t> i = 0; i < actual_size; i++) {
-			new (m_arr + i) dyn_var<T>();
-		}
-		for (static_var<size_t> i = 0; i < actual_size; i++) {
-			m_arr[i].block_var->template setMetadata<int>("allow_escape_scope", 1);
+			new (m_arr + i) T();
 		}
 	}
 
 	template <typename T2, size_t N>
-	void initialize_from_other(const dyn_arr<T2, N> &other) {
+	void initialize_from_other(const array<T2, N> &other) {
 		if (size) {
 			actual_size = size;
 		} else {
 			actual_size = other.actual_size;
 		}
-		m_arr = (dyn_var<T> *)new char[sizeof(dyn_var<T>) * actual_size];
+		m_arr = (T*)new char[sizeof(T) * actual_size];
 		for (static_var<size_t> i = 0; i < actual_size; i++) {
 			if (i < other.actual_size)
-				new (m_arr + i) dyn_var<T>(other[i]);
+				new (m_arr + i) T(other[i]);
 			else
-				new (m_arr + i) dyn_var<T>();
-		}
-		for (static_var<size_t> i = 0; i < actual_size; i++) {
-			m_arr[i].block_var->template setMetadata<int>("allow_escape_scope", 1);
+				new (m_arr + i) T();
 		}
 	}
 
-	dyn_arr(const dyn_arr &other) {
+	array(const array &other) {
 		initialize_from_other(other);
 	}
 	template <typename T2, size_t N>
-	dyn_arr(const dyn_arr<T2, N> &other) {
+	array(const array<T2, N> &other) {
 		initialize_from_other(other);
 	}
 
-	dyn_arr &operator=(const dyn_arr &other) = delete;
+	array &operator=(const array &other) = delete;
 
-	dyn_var<T> &operator[](size_t index) {
+	T &operator[](size_t index) {
 		assert(m_arr != nullptr && "Should call set_size for arrays that don't have a size");
 		return m_arr[index];
 	}
-	const dyn_var<T> &operator[](size_t index) const {
+	const T &operator[](size_t index) const {
 		assert(m_arr != nullptr && "Should call set_size for arrays that don't have a size");
 		return m_arr[index];
 	}
 
-	~dyn_arr() {
+	~array() {
 		if (m_arr) {
 			for (static_var<size_t> i = 0; i < actual_size; i++) {
-				m_arr[i].~dyn_var<T>();
+				m_arr[i].~T();
 			}
 
 			delete[](char *) m_arr;
@@ -109,8 +124,14 @@ public:
 	}
 
 	template <typename T2, size_t N>
-	friend class dyn_arr;
+	friend class array;
 };
+
+template <typename T, size_t N = 0> 
+using dyn_arr = array<dyn_var<T>, N>;
+
+template <typename T, size_t N = 0>
+using arr = array<T, N>;
 
 } // namespace builder
 
