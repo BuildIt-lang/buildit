cbf60b8886ab2499976da727a53f36ad65d7a1d6
Ajay Brahmakshatriya <ajaybr@mit.edu>
2024-12-12 08:45:14 -0500

    Fixed promotion of [0] to * and -> only in specific cases

--- samples/sample60.cpp
+++ samples/sample60.cpp
@@ -3,28 +3,21 @@
 #include "builder/builder_context.h"
 #include "builder/dyn_var.h"
 #include "builder/static_var.h"
-#include "builder/lib/utils.h"
+#include "blocks/rce.h"
 #include <iostream>
 using builder::dyn_var;
 using builder::static_var;
 
-
-template <typename T>
-struct vector: public builder::custom_type<T> {
-	static constexpr const char* type_name = "std::vector";
-	typedef T dereference_type;
-	dyn_var<void(int)> resize = builder::with_name("resize");
-};
+dyn_var<int(void)> create_int = builder::as_global("create_int");
 
 static void bar(void) {
-	dyn_var<vector<vector<int>>> x;
-	x.resize(2);
-	x[0].resize(1);
+	dyn_var<int> x = create_int();
 }
 
 int main(int argc, char *argv[]) {
 	builder::builder_context context;
 	auto ast = context.extract_function_ast(bar, "bar");
+	block::eliminate_redundant_vars(ast);
 	ast->dump(std::cout, 0);
 	block::c_code_generator::generate_code(ast, std::cout, 0);
 	return 0;
